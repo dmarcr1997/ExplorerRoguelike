3b22be17cd8c9dc3d2354c9e2043df73309444b5
Damon Marc Rocha II <dmarcr1997@gmail.com>
2025-05-07 11:10:17 -0500

    Dissolve and health bar materials

--- Source/ExplorerRoguelike/Public/ERTargetDummy.h
+++ Source/ExplorerRoguelike/Public/ERTargetDummy.h
@@ -19,9 +19,6 @@ public:
 	AERTargetDummy();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
 	UPROPERTY(VisibleAnywhere)
 	UERAttributeComponent* AttributeComp;
 
